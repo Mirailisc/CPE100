3228fa68807153abfec878702ec056d64cb4045c
Phubordin Poolnai <mirailisclm@gmail.com>
2023-08-31 10:50:32 +0700

    ni

---
+++ examples/group-assignment/assignment-2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+
+    printf("Enter passenger's age: ");
+    scanf("%d", &age);
+
+    if (age <= 12)
+    {
+        printf("Give kid card to the passenger");
+    }
+    else if (age <= 25)
+    {
+        printf("Give student card to the passenger");
+    }
+    else if (age >= 60)
+    {
+        printf("Give elder card to the passenger");
+    }
+    else
+    {
+        printf("Give normal card to the passenger");
+    }
+
+    return 0;
+}
--- main.c
+++ main.c
@@ -2,7 +2,27 @@
 
 int main()
 {
-	printf("Nigga");
+	int age;
+
+	printf("Enter passenger's age: ");
+	scanf("%d", &age);
+
+	if (age <= 12)
+	{
+		printf("Give kid card to the passenger");
+	}
+	else if (age <= 25)
+	{
+		printf("Give student card to the passenger");
+	}
+	else if (age >= 60)
+	{
+		printf("Give elder card to the passenger");
+	}
+	else
+	{
+		printf("Give normal card to the passenger");
+	}
 
 	return 0;
 }
